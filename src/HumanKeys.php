ded236af8424610a1159f57915dd1086e9a7cab0
Randall Wilk <wilk.randall@gmail.com>
2024-03-06 10:19:55 -0600

    Add more arch tests

--- src/HumanKeys.php
+++ src/HumanKeys.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Rawilk\HumanKeys;
 
 use Rawilk\HumanKeys\Contracts\HumanKeys as HumanKeysContract;
--- tests/ArchTest.php
+++ tests/ArchTest.php
@@ -5,3 +5,23 @@ declare(strict_types=1);
 it('will not use debugging functions')
     ->expect(['dd', 'dump', 'ray', 'var_dump', 'ddd'])
     ->each->not->toBeUsed();
+
+test('strict types are used')
+    ->expect('Rawilk\HumanKeys')
+    ->toUseStrictTypes();
+
+test('strict types are used in tests')
+    ->expect('Rawilk\HumanKeys\Tests')
+    ->toUseStrictTypes();
+
+test('only interfaces are put in the Contracts directory')
+    ->expect('Rawilk\HumanKeys\Contracts')
+    ->toBeInterfaces();
+
+test('only traits are put in the Concerns directory')
+    ->expect('Rawilk\HumanKeys\Concerns')
+    ->toBeTraits();
+
+test('generators are not final')
+    ->expect('Rawilk\HumanKeys\Generators')
+    ->not->toBeFinal();
